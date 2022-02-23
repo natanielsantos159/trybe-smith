ded439c17a0eca987bcc92d77ac44ba48f246f2f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 20:47:12 -0300

    Requisito 2 - Cria função userController.login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import userService from '../services/userService';
+import User from '../interfaces/User';
 
 const create = async (req: Request, res: Response): Promise<Response> => {
   const userInfo = req.body;
   try {
     await userService.create(userInfo);
-    const newToken = process.env.JWT_SECRET && jwt.sign(userInfo, process.env.JWT_SECRET);
+    const newToken = jwt.sign(userInfo, 'myownsecret');
     return res.status(201).json({ token: newToken });
   } catch (err: Error | unknown) {
     return res.status(500).json({ 
@@ -15,6 +16,21 @@ const create = async (req: Request, res: Response): Promise<Response> => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const userInfo: User = req.body;
+  
+  try {
+    await userService.login(userInfo);
+    const newToken = jwt.sign(userInfo, 'myownsecret');
+    return res.status(200).json({ token: newToken });
+  } catch (err: Error | unknown) {
+    return res.status(401).json({ 
+      error: err instanceof Error ? err.message : 'Ocorreu um erro no servidor', 
+    });
+  }
+};
+
 export default {
   create,
+  login,
 };
