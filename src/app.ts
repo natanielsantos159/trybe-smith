85852b73ed1c9c3c489bb80b442e419f6fce392b
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 17:31:37 -0300

    Requisito 1 - Implementa função userController.create

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import validateUserInfo from './middlewares/validateUserInfo';
+import userController from './controllers/userController';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/users', validateUserInfo);
+app.post('/users', validateUserInfo, userController.create);
+
 export default app;
---
+++ src/controllers/userController.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response } from 'express';
+import userService from '../services/userService';
+
+const create = async (req: Request, res: Response): Promise<Response> => {
+  const userInfo = req.body;
+  try {
+    await userService.create(userInfo);
+    const newToken = process.env.JWT_SECRET && jwt.sign(userInfo, process.env.JWT_SECRET);
+    return res.status(201).json({ token: newToken });
+  } catch (err: Error | unknown) {
+    console.log(err instanceof Error ? err.message : 'Ocorreu um erro')
+    return res.status(500).json({ 
+      error: err instanceof Error ? err.message : 'Ocorreu um erro', 
+    });
+  }
+};
+
+export default {
+  create,
+};
