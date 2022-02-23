8987b5fe1cd6b92fc2f8d0332936c3e5a7dfc799
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 20:45:29 -0300

    Requisito 2 - Cria rota POST /login

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import validateUserInfo from './middlewares/validateUserInfo';
 import userController from './controllers/userController';
 
+import validateLogin from './middlewares/validateLogin';
+
 const app = express();
 
 app.use(express.json());
 
 app.post('/users', validateUserInfo, userController.create);
+
+app.post('/login', validateLogin, userController.login);
 
 export default app;
