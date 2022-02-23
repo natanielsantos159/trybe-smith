31e3ac4101b0fcbb0092269c267e5b6084ff3a7c
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 19:14:36 -0300

    Refatora Validação de informações de usuario

--- src/middlewares/validateUserInfo.ts
+++ src/middlewares/validateUserInfo.ts
@@ -3,28 +3,25 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../interfaces/User';
 
 const schema = Joi.object({
-  username: Joi.string().min(3).required().messages({
-    'string.min': '{{#label}} must be longer than 2 characters',
-  }),
-  classe: Joi.string().min(3).required().messages({
-    'string.min': '{{#label}} must be longer than 2 characters',
-  }),
-  level: Joi.number().min(1).required().messages({
-    'number.min': '{{#label}} must be greater than 0',
-  }),
-  password: Joi.string().min(8).required().messages({
-    'string.min': '{{#label}} must be longer than 7 characters',
-  }),
+  username: Joi.string().label('Username').min(3).required()
+    .messages({
+      'string.min': '{{#label}} must be longer than 2 characters',
+    }),
+  classe: Joi.string().min(3).label('Classe').required()
+    .messages({
+      'string.min': '{{#label}} must be longer than 2 characters',
+    }),
+  level: Joi.number().min(1).label('Level').required()
+    .messages({
+      'number.min': '{{#label}} must be greater than 0',
+    }),
+  password: Joi.string().min(8).label('Password').required()
+    .messages({
+      'string.min': '{{#label}} must be longer than 7 characters',
+    }),
 });
 
-const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
-const getErrorCode = (type: string):number => {
-  if (type === 'string.required' || type === 'any.required') {
-    return 400;
-  }
-  return 422;
-};
+const getErrorCode = (type: string):number => (type.includes('required') ? 400 : 422);
 
 const validateUserInfo = async (
   req: Request,
@@ -37,11 +34,9 @@ const validateUserInfo = async (
     await schema.validateAsync(userInfo, { convert: false });
     next();
   } catch (err: ValidationError | unknown) {
-    if (err instanceof ValidationError && typeof err.details[0].path[0] === 'string') {
+    if (err instanceof ValidationError) {
       const { type } = err.details[0];
-      const property = err.details[0].path[0];
-      const newMessage = err.message.replace(`"${property}"`, capitalize(property));
-      res.status(getErrorCode(type)).json({ error: newMessage });
+      res.status(getErrorCode(type)).json({ error: err.message.replace(/"/g, '') });
     }
   }
 };
