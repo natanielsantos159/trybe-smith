ac23087f5d70cf80a59f875d7c8c53bb5db93836
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 15:14:58 -0300

    Requisito 1 - Cria rota POST /users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import validateUserInfo from './middlewares/validateUserInfo';
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/users', validateUserInfo);
 export default app;
