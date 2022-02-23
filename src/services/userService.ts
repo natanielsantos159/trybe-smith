88b2d68a97e1093714b1fdba6b6d580a59449a85
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-23 20:47:28 -0300

    Requisito 2 - Cria função userService.login

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,6 +5,11 @@ const create = async (userInfo: User): Promise<void> => {
   await userModel.create(userInfo);
 };
 
+const login = async (userInfo: User): Promise<void> => {
+  await userModel.login(userInfo);
+};
+
 export default {
   create,
+  login,
 };
