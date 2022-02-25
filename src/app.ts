949354c649c7d24f3cfc91b750e53685c83490c6
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-25 18:48:14 -0300

    Requisito 5 - Cria rota POST /orders

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import validateUserInfo from './middlewares/validateUserInfo';
 import userController from './controllers/userController';
 import productController from './controllers/productController';
+import orderController from './controllers/orderController';
 
 import validateLogin from './middlewares/validateLogin';
 import validateProductInfo from './middlewares/validateProductInfo';
 import validateJWT from './middlewares/validateJWT';
+import validateOrder from './middlewares/validateOrder';
 
 const app = express();
 
@@ -20,5 +22,7 @@ app.use(validateJWT);
 app.post('/products', validateProductInfo, productController.create);
 
 app.get('/products', productController.getAll);
+
+app.post('/orders', validateOrder, orderController.create);
 
 export default app;
