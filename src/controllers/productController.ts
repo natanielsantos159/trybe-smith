0fe2a842b8b313b2932587e74f5ae76d9697983e
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-25 17:24:37 -0300

    Requisito 4 - Implementa função productController.getAll

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,21 +1,24 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import Product from '../interfaces/Product';
 import productService from '../services/productService';
 
 const create = async (req: Request, res: Response) => {
   const productInfo: Product = req.body;
-  
-  const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ error: 'Token not found' });
-
-  const data = jwt.decode(authorization);
-  if (!data) return res.status(401).json({ error: 'Invalid token' });
 
   const createdProduct = await productService.create(productInfo);
   return res.status(201).json({ item: createdProduct });
 };
 
+const getAll = async (_req: Request, res: Response) => {
+  try {
+    const allProducts: Product[] = await productService.getAll();
+    return res.status(200).json(allProducts);
+  } catch (err) {
+    return res.status(500).json({ error: 'Ocorreu um erro no servidor' });
+  }
+};
+
 export default {
   create,
+  getAll,
 };
