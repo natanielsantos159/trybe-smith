55ab8a5bf1ee13f98725bbccb3a70c69c06ad404
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-01 15:50:15 -0300

    Requisito 6 - Implementa model orderModel.getById

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -1,4 +1,5 @@
 import { ResultSetHeader, FieldPacket } from 'mysql2';
+import Order from '../interfaces/Order';
 import connection from './connection';
 
 const create = async (userId: number): Promise<number> => {
@@ -10,6 +11,16 @@ const create = async (userId: number): Promise<number> => {
   return result.insertId;
 };
 
+const getById = async (id: number) => {
+  const [rows] = await connection
+    .execute(
+      'SELECT * FROM Trybesmith.Orders WHERE id = ?',
+      [id],
+    ) as [Order[], FieldPacket[]];
+  return rows[0];
+};
+
 export default {
   create,
+  getById,
 };
