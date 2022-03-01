2c4ea8adbbf7954cb82ddc2d5cdc15f7944e426f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-03-01 15:47:07 -0300

    Requisito 6 - Implementa controller orderController.getById

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -21,6 +21,19 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
+const getById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const productInfo = await orderService.getById(Number(id));
+    return res.status(200).json(productInfo);
+  } catch (err: Error | unknown) {
+    if (err instanceof Error) {
+      return res.status(404).json({ error: err.message });
+    }
+  }
+};
+
 export default {
   create,
+  getById, 
 };
